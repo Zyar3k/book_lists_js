3bba100ef3264087651abf489e5efd766af94753
Arkadiusz Zygan <a.zygan@gmail.com>
2020-07-21 18:16:48 +0200

    Add rendering books

--- js/script.js
+++ js/script.js
@@ -3,18 +3,29 @@ console.log(bookData);
 
 bookList.map((book, index) => {
 
-  const {title, author, page, key} = book;
-  console.log(book);
-
+  const {title, author, page, link} = book;
 
   const bookWrapper = document.querySelector('.bookList');
   const bookBox = document.createElement('div');
   bookBox.classList.add('bookBox');
   
   const bookBoxContent = `
-        <div>${key+1}- ${title}</div>
-        <div>autor - ${author}</div>
-        <div>stron - ${page}</div>
+    <div class="bookInfo">
+      <div class="infoDet">
+        <div class="bookNo">${index+1}.</div>
+        <a href="${link}" target='_blank' rel='noopener noreferrer'><i class="fas fa-info-circle"></i></a>
+      </div>
+      <div class="pageWrapper">
+        <div class="bookPage"><i class="fas fa-book-open"></i>${page}</div>
+        <div class="bookTime"><i class="far fa-clock"></i>5h30min</div>
+      </div>
+    </div>
+    <div class="bookTitle">${title}</div>
+    <div class="bookAuthor">${author}</div>
+    <div class="myInfo">
+      <i class="fas fa-check-double"></i>
+      <i class="fas fa-tablet-alt"></i>
+    </div>
   `;
   bookBox.innerHTML = bookBoxContent;
   bookWrapper.append(bookBox);
